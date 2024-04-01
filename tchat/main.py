2d4540611933015bcbf84e0674f48a6684188a59
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2024-04-01 15:58:49 +0200

    First complete version
    Added memory and chat summary memory so this gives transient memory of the current conversation.

--- tchat/main.py
+++ tchat/main.py
@@ -1,24 +1,39 @@
 import os 
 api_key = os.environ.get("OPENAI_API_KEY")
 
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
 
 
 chat = ChatOpenAI(openai_api_key=api_key, model="gpt-3.5-turbo")
+memory = ConversationSummaryMemory(
+    memory_key="messages",
+    return_messages=True,
+    llm=chat
+    #chat_memory=FileChatMessageHistory("messages.json")
+    )
 
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
-    messages=[HumanMessagePromptTemplate.from_template("{content}")]
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
     content = input(">> ")
+
+    if content == "exit()":
+        print("Bye bye!")
+        break
+
     result = chain.invoke({"content": content})
     print(result["text"])
