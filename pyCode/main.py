18fbd70271fa88ce25125511f190cd704de8581b
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2024-04-01 12:12:56 +0200

    Initial behaviour
    Simple chain to accept input arguments with a language and task and output code function in a language of choice

--- pyCode/main.py
+++ pyCode/main.py
@@ -1,11 +1,36 @@
 import os
+import argparse
 
 api_key = os.environ.get("OPENAI_API_KEY")
 
 from langchain_openai import OpenAI
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 llm = ChatOpenAI(openai_api_key=api_key, model="gpt-3.5-turbo")
 
-result = llm.invoke("Write a very very short poem")
-print(result.content)
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+
+args = parser.parse_args()
+
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt
+)
+
+result = code_chain.invoke(
+    {
+        "language": args.language,
+        "task": args.task
+    }
+)
+
+print(result["text"])
